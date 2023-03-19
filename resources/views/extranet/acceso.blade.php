6ed4c9798d0f990cfd95a2bc668544c9177cbecc
cesarmaya1006 <cesarmaya1006@gmail.com>
2023-03-19 18:49:36 -0500

    Control de cambios  19-03-2023

--- resources/views/extranet/acceso.blade.php
+++ resources/views/extranet/acceso.blade.php
@@ -1,57 +1,186 @@
-@extends("extranet.plantilla")
+@extends('extranet.plantilla')
 <!-- ************************************************************* -->
 @section('css_pagina')
-    <link rel="stylesheet" href="{{ asset('css/extranet/login.css') }}">
+<link rel="stylesheet" href="{{ asset('css/extranet/preloader.css') }}">
+<link rel="stylesheet" href="{{ asset('css/extranet/style.css') }}">
+<link rel="stylesheet" href="{{ asset('css/extranet/style.customer.css') }}">
+<link rel="stylesheet" href="{{ asset('css/extranet/style.min.css') }}">
+
+<script src="{{ asset('js/extranet/jquery-2.0.3.min.js.descarga') }}"></script>
+<script src="{{ asset('js/extranet/jquery.validate.min.js.descarga') }}"></script>
+<script src="{{ asset('js/extranet/bootstrap.min.js.descarga') }}"></script>
+
+<script async="" src="{{ asset('login/js') }}"></script>
+<script src="{{ asset('login/ELjwtGhd7DT3C3WQ9X-IuRh2tvc.js.descarga') }}"></script>
+<script src="{{ asset('login/G5uXR5872C56479CVNt5wXA9Wzc.js.descarga') }}"></script>
+<link rel="stylesheet" href="{{ asset('login/bootstrap.min.css') }}" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<!-- <link href="./libs/architectui-theme/main.css" rel="stylesheet"> -->
+<link rel="stylesheet" href="{{ asset('login/style.css') }}">
+<link rel="stylesheet" href="{{ asset('login/preloader.css') }}">
+<link rel="stylesheet" href="{{ asset('login/style.min.css') }}">
+<link rel="stylesheet" href="{{ asset('login/style.customer.css') }}">
+<script src="{{ asset('login/jquery-2.0.3.min.js.descarga') }}"></script>
+<script src="{{ asset('login/jquery.validate.min.js.descarga') }}"></script>
+<script src="{{ asset('login/bootstrap.min.js.descarga') }}"></script>
+
 @endsection
 @section('cuerpo_pagina')
-    <div class="container index">
-        <div class="container mt-3">
-            <div class="row d-flex justify-content-center mt-3">
-                <div class="col-11 col-md-6">
-                    @include('includes.error-form')
-                    @include('includes.mensaje')
+    <div id="preloader" class="preloader" style="display: none;">
+        <div class="loader-content">
+            <img src="{{ asset('imagenes/sistema/logo.svg') }}" alt="">
+            <div class="progress">
+                <div class="indeterminate"></div>
+            </div>
+            <h5 class="msg">Cargando</h5>
+        </div>
+    </div>
+    <section>
+        <div class="peers ai-s fxw-nw h-100vh" style="background-image: url(&quot;{{ asset('imagenes/sistema/bg.jpg') }}&quot;); background-size: cover;">
+            <div class="d-n@sm- peer peer-greed h-100 pos-r bgr-n bgpX-c bgpY-c bgsz-cv">
+                <div class="pos-a positionXY">
+                    <img src="{{ asset('imagenes/sistema/logo.svg') }}" class="pos-a positionXY logo-cpe-login" alt="logo-cpe">
                 </div>
             </div>
-            <div class="row justify-content-center" style="font-size: 0.8em;">
-                <div class="col-10 col-sm-8 col-md-6 mt-5">
-                    <div class="card">
-                        <div class="card-body">
-                            <img src="{{ asset('imagenes/sistema/logofull.png') }}" class="rounded mx-auto d-block" alt="...">
-                            <div class="card-text mt-3">
-                                <form action="{{ route('login') }}" method="post" autocomplete="off">
-                                    @method('post')
-                                    @csrf
-                                    <div class="form-row">
-                                        <div class="col-md-12 mb-3">
-                                            <input type="text" class="form-control form-control-sm" id="usuario"
-                                                name="usuario" placeholder="Usuario*" required>
-                                        </div>
-                                        <div class="col-md-12 mb-3">
-                                            <input type="password" class="form-control form-control-sm" id="password"
-                                                name="password" placeholder="Contraseña*" required>
-                                        </div>
-                                    </div>
-                                    <div class="d-flex justify-content-around">
-                                        <div class="centrar-items">
-                                            <button class="mt-2 btn btn-primary" type="submit">Iniciar sesiónes</button>
-                                        </div>
-                                        <div class="centrar-items">
-                                            <a href="{{ route('registro_ini') }}" class="mt-2 btn btn-primary">Registrarse</a>
-                                        </div>
-                                    </div>
-                                    <div class="centrar-items mt-4">
-                                        <p><a class="card-text h6" href="{{ route('solicitar_password') }}">Olvidé mi nombre
-                                                de usuario o contraseña bromelias</a></p>
-                                    </div>
-                                </form>
+            <div class="col-12 col-md-4 peer h-100 bgc-white scrollable pos-r"
+                style="min-width: 320px; border-left: 3px solid #D8D8D8; padding: 0;">
+                <!-- <h4 class="fw-300 c-grey-900 mB-40">Login</h4> -->
+                <div class="header-login bg-white">
+                    <div class="box-img">
+                        <img src="{{ asset('imagenes/sistema/logo-cronos.svg') }}" alt="Cronos">
+                    </div>
+                    <div class="box-img hidden-sm-up">
+                        <img src="{{ asset('imagenes/sistema/logo.svg') }}" alt="logo">
+                    </div>
+                    <div class="box-img">
+                        <img src="{{ asset('imagenes/sistema/logo-thanos.svg') }}" alt="Thanos">
+                    </div>
+                </div>
+
+                <div class="body-login pX-40 pY-40">
+                    <h2 class="bigintro">SISTEMA DE <br><span>GESTIÓN ESTRATÉGICA</span></h2>
+
+                    <div id="login" class="session visible">
+                        <form method="post" accept-charset="utf-8" class="form-login" role="form" id="frmLogin"
+                            action="https://cronoscpeqa.linktic.com/users/login" data-gtm-form-interact-id="0">
+                            <div style="display:none;"><input type="hidden" name="_method" value="POST"></div>
+                            <div class="input-group mb-3 input-user">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="basic-addon1"><i class="icon-head"></i></span>
+                                </div>
+                                <input name="username" type="text" class="form-control" placeholder="Usuario"
+                                    aria-label="Username" aria-describedby="basic-addon1"
+                                    data-gtm-form-interact-field-id="0">
                             </div>
+                            <div class="input-group mb-3 input-pass">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="basic-addon1"><i class="icon-key4"></i></span>
+                                </div>
+                                <input name="password" type="password" class="input-password form-control"
+                                    placeholder="Contraseña" aria-label="Username" aria-describedby="basic-addon1"
+                                    data-gtm-form-interact-field-id="1">
+                                <span class="btn-show-pass input-group-addon cursor-pointer"><i
+                                        class="icon-eye-off"></i></span>
+                            </div>
+
+                            <div class="mensaje">
+                            </div>
+
+                            <div class="form-group">
+                                <div class="peers ai-c jc-sb fxw-nw">
+                                    <div class="peer box-btn-login">
+                                        <button class="btn btn-primary text-uppercase">Iniciar sesion</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                        <div class="login-helpers">
+                            <a href="https://cronoscpeqa.linktic.com/users/login#"
+                                onclick="swapScreen(&#39;forgot&#39;);return false;" style="color:#FFF;">Reset
+                                Password?</a> <br>
+                        </div>
+                    </div>
+
+                    <div id="forgot" class="session invisible">
+                        <form class="form-login" role="form" id="frmRestablecer">
+                            <h4 class="" style="text-align: center; color: #FFF;">Reset Password</h4>
+                            <div class="divide-40"></div>
+                            <div class="input-group mb-3 input-user">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text" id="basic-addon1"><i class="icon-head"></i></span>
+                                </div>
+                                <input name="username" type="text" class="form-control" placeholder="Usuario"
+                                    aria-label="Username" aria-describedby="basic-addon1">
+                            </div>
+
+                            <div class="form-group">
+                                <div class="peers ai-c jc-sb fxw-nw">
+                                    <div class="peer box-btn-login">
+                                        <button type="submit" class="btn btn-primary text-uppercase">Restablecer</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                        <div id="mensaje"></div>
+                        <div class="login-helpers">
+                            <a href="https://cronoscpeqa.linktic.com/users/login#"
+                                onclick="swapScreen(&#39;login&#39;);return false;" style="color:#FFF;">Volver para
+                                iniciar sesión</a> <br>
                         </div>
                     </div>
                 </div>
+
+                <div class="footer-login">
+                    <img src="{{ asset('imagenes/sistema/logo-linktic.svg') }}" alt="LinkTic">
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <!-- Modal fullscreen -->
+    <div class="modal modal-fullscreen fade" id="modal-fullscreen" tabindex="-1" role="dialog"
+        aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close btn btn-circle btn-default" data-dismiss="modal"><span
+                            aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+
+                </div>
+                <div class="modal-body">
+                    <div class="modal-search">
+                        <form>
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <span class="input-group-addon"><i class="fa fa-search"></i></span>
+                                    <input type="text" class="form-control input-lg" placeholder="Buscar">
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div> <!-- MODALES -->
+    <div id="popup" style="display: none;">
+        <div class="content-popup">
+            <div class="close"><a href="https://cronoscpeqa.linktic.com/users/login#" id="close"><img
+                        src="{{ asset('login/close.png') }}" alt=""></a></div>
+            <div>
+                <h3>Características de una contraseña segura</h3>
+                <ul>
+                    <li>Debe incluir números.</li>
+                    <li>Utilice una combinación de letras mayúsculas y minúsculas.</li>
+                    <li>Incluya caracteres especiales.</li>
+                    <li>Cualquiera de los siguientes caracteres: (- * ? ! @ # $ / () {} = . , ; : )</li>
+                    <li>Tenga una longitud mayor o igual a 8 caracteres.</li>
+                    <li>No debe tener espacios en blanco.</li>
+                </ul>
+                <span><strong>Ejemplo: </strong>Linktic2017*, Sig*2017, pa$$W0rd*</span>
             </div>
         </div>
     </div>
 @endsection
 @section('script_pagina')
-
+<script src=""></script>
 @endsection
---
+++ resources/views/extranet/acceso2.blade.php
@@ -0,0 +1,62 @@
+@extends("extranet.plantilla")
+<!-- ************************************************************* -->
+@section('css_pagina')
+    <link rel="stylesheet" href="{{ asset('css/extranet/login.css') }}">
+@endsection
+@section('cuerpo_pagina')
+    <div class="container index">
+        <div class="container mt-3">
+            <div class="row d-flex justify-content-center mt-3">
+                <div class="col-11 col-md-6">
+                    @include('includes.error-form')
+                    @include('includes.mensaje')
+                </div>
+            </div>
+            <div class="row justify-content-center" style="font-size: 0.8em;">
+                <div class="col-10 col-sm-8 col-md-6 mt-5">
+                    <div class="card">
+                        <div class="card-body">
+                            <img src="{{ asset('imagenes/sistema/logofull.png') }}" class="rounded mx-auto d-block" alt="...">
+                            <div class="row">
+                                <div class="col-12 text-center">
+                                    <div class="h1">CRONOS</div>
+                                </div>
+                            </div>
+                            <div class="card-text mt-3">
+                                <form action="{{ route('login') }}" method="post" autocomplete="off">
+                                    @method('post')
+                                    @csrf
+                                    <div class="form-row">
+                                        <div class="col-md-12 mb-3">
+                                            <input type="text" class="form-control form-control-sm" id="usuario"
+                                                name="usuario" placeholder="Usuario*" required>
+                                        </div>
+                                        <div class="col-md-12 mb-3">
+                                            <input type="password" class="form-control form-control-sm" id="password"
+                                                name="password" placeholder="Contraseña*" required>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex justify-content-around">
+                                        <div class="centrar-items">
+                                            <button class="mt-2 btn btn-primary" type="submit">Iniciar sesiónes</button>
+                                        </div>
+                                        <div class="centrar-items">
+                                            <a href="{{ route('registro_ini') }}" class="mt-2 btn btn-primary">Registrarse</a>
+                                        </div>
+                                    </div>
+                                    <div class="centrar-items mt-4">
+                                        <p><a class="card-text h6" href="{{ route('solicitar_password') }}">Olvidé mi nombre
+                                                de usuario o contraseña bromelias</a></p>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+@section('script_pagina')
+
+@endsection
